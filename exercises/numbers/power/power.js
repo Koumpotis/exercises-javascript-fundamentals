fa7438155853da9b6dd4dc71f6b5c7133d2044b9
Christos Koumpotis <59896173+Koumpotis@users.noreply.github.com>
2020-01-18 20:59:20 -0800

    Update power.js

--- exercises/numbers/power/power.js
+++ exercises/numbers/power/power.js
@@ -10,31 +10,33 @@
  * @param {number} exponent The exponent (a positive integer)
  * @returns {number} The base raised to the power of the exponent
  */
+
+
 function power(base, exponent){
-    if (exponent=0){
+    if (exponent===0){
       return 1;
-    } else {
-    if (exponent=1) {
+    } 
+    
+    else if (exponent===1){
       return base; 
-    } else { 
-      for (num=1; num=exponent;num = num*exponent)
-      return num 
+    } 
+    
+    else { 
+      let answer=1;
+      for (let num=1; num<=exponent; num++){
+         answer = answer*base; 
+       } 
+       return answer;
     }
   }
-}
+
+console.log(power(4,2));
       if (require.main === module) {
         console.log('Running sanity checks for power:');
       //console.log(power(1,2));
         console.log(power(1, 1) === 1);
         console.log(power(1, 2) === 1);
-      
+      console.log(power(5,2) ===25);
         console.log(power(-1, 1) === -1);
         console.log(power(-1, 3) === -1);
-      
-         //Your sanity checks go here
-        // What should happen if base is 0? If exponent is 0?
-      }
-      
-      module.exports = power;
-    
   
