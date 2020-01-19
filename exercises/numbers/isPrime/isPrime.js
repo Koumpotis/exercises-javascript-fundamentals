5556e2d2beeb9d3993d415cb994cfba01fc488f9
Christos Koumpotis <59896173+Koumpotis@users.noreply.github.com>
2020-01-18 23:49:32 -0800

    Update isPrime.js

--- exercises/numbers/isPrime/isPrime.js
+++ exercises/numbers/isPrime/isPrime.js
@@ -11,6 +11,37 @@
  * @returns {boolean} True if num is prime and false otherwise
  */
 function isPrime(num) {
+  let count=0;
+  
+
+  if (num===1){
+    return false;
+ }
+
+
+  for (let a=1; a<num; a++){
+    //answer= num/a
+  if (num % a ===0 ){
+  count = count + 1;
+  } 
+  
+  else {
+    count=count
+  } }
+  
+
+
+if (count>1) { 
+  return false; 
+} 
+
+else {
+  return true;
+}
+}
+
+  
+  
   /*
     Your code goes here.
 
@@ -20,14 +51,16 @@ function isPrime(num) {
     and use pen/paper, index cards, etc. — anything that helps you think
     about it without getting stuck in JavaScript syntax.
   */
-}
 
 if (require.main === module) {
   console.log('Running sanity checks for isPrime:');
-
+ 
   console.log(isPrime(1) === false);
   console.log(isPrime(2) === true);
   console.log(isPrime(4) === false);
+  console.log(isPrime(6) === false);
+  console.log(isPrime(7) === true);
+  console.log(isPrime(11) === true);
 
   // Your own sanity checks go here
 }
