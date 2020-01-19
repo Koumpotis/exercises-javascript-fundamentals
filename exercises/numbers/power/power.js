78d26f05f786f80e4a6c0334280492444bea68f6
Christos Koumpotis <59896173+Koumpotis@users.noreply.github.com>
2020-01-18 20:23:23 -0800

    Update power.js

--- exercises/numbers/power/power.js
+++ exercises/numbers/power/power.js
@@ -8,33 +8,33 @@
  *
  * @param {number} base The base
  * @param {number} exponent The exponent (a positive integer)
- * @returns {number} The base raised to the power of the exopnent
+ * @returns {number} The base raised to the power of the exponent
  */
-function power(base, exponent) {
-  /*
-    Your code goes here.
-
-    Remember, exponent is a _positive integer_.
-
-    JavaScript has a built-in Math.pow function; don't use it!
-    Write your own version. Remember, exponent is guaranteed to be a positive integer.
-
-    That means you don't have to deal with, e.g., power(2, 1.5).
-  */
+function power(base, exponent){
+    if (exponent=0){
+      return 1;
+    } else {
+    if (exponent=1) {
+      return base; 
+    } else { 
+      for (num=1; num=exponent;num = num*exponent)
+      return num 
+    }
+  }
 }
-
-if (require.main === module) {
-  console.log('Running sanity checks for power:');
-
-  console.log(power(1, 1) === 1);
-  console.log(power(1, 2) === 1);
-
-  console.log(power(-1, 1) === -1);
-  console.log(power(-1, 2) === 1);
-  console.log(power(-1, 3) === -1);
-
-  // Your sanity checks go here
-  // What should happen if base is 0? If exponent is 0?
-}
-
-module.exports = power;
+      if (require.main === module) {
+        console.log('Running sanity checks for power:');
+      //console.log(power(1,2));
+        console.log(power(1, 1) === 1);
+        console.log(power(1, 2) === 1);
+      
+        console.log(power(-1, 1) === -1);
+        console.log(power(-1, 3) === -1);
+      
+         //Your sanity checks go here
+        // What should happen if base is 0? If exponent is 0?
+      }
+      
+      module.exports = power;
+    
+  
