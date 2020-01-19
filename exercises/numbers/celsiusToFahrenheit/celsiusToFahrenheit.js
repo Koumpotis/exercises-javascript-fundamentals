725331906d2947860c8e1a9b367d84b9bb192c51
Christos Koumpotis <59896173+Koumpotis@users.noreply.github.com>
2020-01-19 14:53:26 -0800

    Update celsiusToFahrenheit.js

--- exercises/numbers/celsiusToFahrenheit/celsiusToFahrenheit.js
+++ exercises/numbers/celsiusToFahrenheit/celsiusToFahrenheit.js
@@ -4,14 +4,21 @@
  * @param {number} temp - The temperature in Celsius
  * @returns {boolean} The temperature in Fahrenheit
  */
+// T(°C) = (T(°F) - 32) × 5/9
+// T(°F) = T(°C) × 9/5 + 32
 function celsiusToFahrenheit(temp) {
-  // This is your job. :).
+  let Fahrenheit = temp * 9/5 +32
+  return Fahrenheit;
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for celsiusToFahrenheit:');
+console.log(celsiusToFahrenheit(30)===86);
+console.log(celsiusToFahrenheit(40)===104);
+console.log(celsiusToFahrenheit(32)===89.6);
+console.log(celsiusToFahrenheit(12)===53.6);
 
-  // Your sanity checks go here.
+// Your sanity checks go here.
 }
 
 module.exports = celsiusToFahrenheit;
