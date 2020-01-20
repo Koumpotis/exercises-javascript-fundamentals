bad96d3bac3d1242e2fee0f8fb8885199b195c94
Christos Koumpotis <59896173+Koumpotis@users.noreply.github.com>
2020-01-19 19:27:16 -0800

    Update letterGrade.js

--- exercises/branching-conditionals/letterGrade/letterGrade.js
+++ exercises/branching-conditionals/letterGrade/letterGrade.js
@@ -14,14 +14,45 @@
  * @return {string} The corresponding letter grade for the given percentage grade
  */
 function letterGrade(percentGrade) {
-  // This is your job. :)
-}
-
+ if (percentGrade<60){
+   return 'F';
+ } else if (percentGrade>60 & percentGrade<62) {
+   return 'D-';
+ } else if (percentGrade>62 & percentGrade<66){
+   return 'D';
+ } else if (percentGrade>66 & percentGrade<69) {
+   return 'D+';
+  } else if (percentGrade>69 & percentGrade<72) {
+    return 'C-';
+  } else if (percentGrade>72 & percentGrade<76){
+    return 'C';
+  } else if (percentGrade>76 & percentGrade<79) {
+    return 'C+';
+  } else if (percentGrade>79 & percentGrade<82) {
+    return 'B-';
+  } else if (percentGrade>82 & percentGrade<86){
+    return 'B';
+  } else if (percentGrade>86 & percentGrade<89) {
+    return 'B+';
+  } else if (percentGrade>89 & percentGrade<92) {
+    return 'A-';
+  } else if (percentGrade>92 & percentGrade<96){
+    return 'A';
+  } else if (percentGrade>96 & percentGrade<100) {
+    return 'A+';
+  } }
 if (require.main === module) {
   console.log('Running sanity checks for letterGrade:');
+console.log(letterGrade(50)==='F');
+console.log(letterGrade(65)==='D');
+console.log(letterGrade(99)==='A+');
+console.log(letterGrade(81)==='B-');
+console.log(letterGrade(95)==='A');
+console.log(letterGrade(70)==='C-');
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
 
 module.exports = letterGrade;
+
