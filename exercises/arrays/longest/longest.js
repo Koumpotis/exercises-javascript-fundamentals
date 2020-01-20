0209093f5f72d5d0aa40b24489217f6342c302b6
Christos Koumpotis <59896173+Koumpotis@users.noreply.github.com>
2020-01-19 21:18:22 -0800

    Update longest.js

--- exercises/arrays/longest/longest.js
+++ exercises/arrays/longest/longest.js
@@ -8,10 +8,22 @@
  */
 function longest(array) {
   // This is your job. :)
-}
+  let longestSoFar = array[0];
+
+  for (let element of array) {
+    if (element.length> longestSoFar.length) {
+      longestSoFar = element;
+    }
+  }
+
+  return longestSoFar;}
 
 if (require.main === module) {
   console.log('Running sanity checks for longest:');
+console.log(longest(['dd','dddd']));
+console.log(longest(['dd','dd']));
+console.log(longest(['dd','dddd', 'dddd','ddddd']));
+console.log(longest(['dd','dddd', 'dd', 'd']));
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
