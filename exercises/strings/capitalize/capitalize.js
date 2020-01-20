840a243f3cb6a63fe9e8147bc76ed8f62e02fb6c
Christos Koumpotis <59896173+Koumpotis@users.noreply.github.com>
2020-01-19 18:07:09 -0800

    Update capitalize.js

--- exercises/strings/capitalize/capitalize.js
+++ exercises/strings/capitalize/capitalize.js
@@ -10,15 +10,22 @@
  * @returns {string} A capitalized copy of the string
  */
 
-function capitalize(num) {
-  // This is your job. :)
+function capitalize(word) {
+  let upped= word.slice(0,1).toUpperCase();
+  return upped + word.slice(1);
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for capitalize:');
+console.log(capitalize('mom'));
+console.log(capitalize('christos')=== 'Christos');
+console.log(capitalize('coding')==='Coding');
+console.log(capitalize('Faith')==='Faith');
+
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
 
 module.exports = capitalize;
+
