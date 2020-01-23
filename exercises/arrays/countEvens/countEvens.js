8eb996967ee094cd9d3e0fa0eebdbd4dfaaa7fd6
Christos Koumpotis <59896173+Koumpotis@users.noreply.github.com>
2020-01-22 20:24:18 -0800

    Update countEvens.js

--- exercises/arrays/countEvens/countEvens.js
+++ exercises/arrays/countEvens/countEvens.js
@@ -12,12 +12,19 @@
  * @returns {number} The count of even integers in the array
  */
 function countEvens(array) {
-  // This is your job. :)
+let count=0;
+  for (a=0; a<array.length; a++){
+  if (array[a]%2===0){
+    count+=1
+  }
+} return count;
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for countEvens:');
-
+console.log(countEvens([1,2,3,4,5,6,7,8]));
+console.log(countEvens([1,2,2,2,3,5]));
+console.log(countEvens([1,3,5]));
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
