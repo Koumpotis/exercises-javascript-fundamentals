f8a663f45c6ef8b51e48a53b7f50fb91480be826
Christos Koumpotis <59896173+Koumpotis@users.noreply.github.com>
2020-01-22 20:27:17 -0800

    Update countOdds.js

--- exercises/arrays/countOdds/countOdds.js
+++ exercises/arrays/countOdds/countOdds.js
@@ -11,12 +11,21 @@
  * @param {number[]} array - An array of numbers
  * @returns {number} The count of even integers in the array
  */
-function countOdds(array) {
-  // This is your job. :)
-}
 
-if (require.main === module) {
-  console.log('Running sanity checks for countOdds:');
+function countOdds(array) {
+  let count=0;
+    for (a=0; a<array.length; a++){
+    if (array[a]%2!==0){
+      count+=1
+    }
+  } return count;
+  }
+  
+  if (require.main === module) {
+    console.log('Running sanity checks for countOdds:');
+  console.log(countOdds([1,2,3,4,5,6,7,8]));
+  console.log(countOdds([1,2,2,2,3,5]));
+  console.log(countOdds([2,4,6]));
 }
 
 module.exports = countOdds;
